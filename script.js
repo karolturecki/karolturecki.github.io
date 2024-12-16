e1fb74952215572f11b7802fe113548b6363dfbe
karol <karol.turecki@infotech.edu.pl>
2024-12-16 19:30:22 +0100

    Ogarnac obrazki
    # Conflicts:
    #	index.html

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 setInterval(function () {
     const date = new Date();
-    const czas = "Aktualny czas: " + date.getHours() + '-' +  date.getMinutes() +'-'+ date.getSeconds();
+    const czas = "Aktualny czas: " + date.getHours() + 'h-' +  date.getMinutes() +'m-'+ date.getSeconds() + "s";
     document.getElementById('czas').innerHTML = czas;
-} ,1000)``
+} ,1000)
